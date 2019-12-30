1f98412c01e3a9b37a1b6bdbdf00d97893ac4bc4
fengbujue <619577235@qq.com>
2019-12-30 23:37:22 +0800

    修改了目录结构

---
+++ src/main/java/com/zyj/springboot_test/test/java/testThread/thread_base/MethodsOfStopThread.java
@@ -0,0 +1,47 @@
+package com.zyj.springboot_test.test.java.testThread.thread_base;
+
+public class MethodsOfStopThread {
+    public static void main(String[] args) throws Exception{
+
+    }
+
+
+    /**
+     * 测试stop方法
+     * 该方法回破坏操作的原子性
+     */
+    private static void testStopMethod() throws Exception{
+        MyThread thread = new MyThread();
+        thread.start();
+
+        Thread.sleep(1000);     // 休眠1秒，确保i变量自增成功
+//        thread.interrupt();
+        thread.stop();
+
+        while (thread.isAlive()) {}  // 确保线程已经终止
+        thread.print();     // 输出结果
+    }
+}
+
+
+class MyThread extends Thread {
+    private int i = 0, j = 0;
+
+    @Override
+    public void run() {
+        synchronized (this) {
+            ++i;
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            ++j;
+        }
+        System.out.println("锁释放。。。");
+    }
+
+    public void print() {
+        System.out.println("i=" + i + " j=" + j);
+    }
+}
--- src/main/java/com/zyj/springboot_test/test/java/testThread/thread_base/ThreadStatusTest.java
+++ src/main/java/com/zyj/springboot_test/test/java/testThread/thread_base/ThreadStatusTest.java
@@ -1,4 +1,4 @@
-package com.zyj.springboot_test.test.java.test_thread_status;
+package com.zyj.springboot_test.test.java.testThread.thread_base;
 
 public class ThreadStatusTest {
     public static void main(String[] args) throws Exception{
